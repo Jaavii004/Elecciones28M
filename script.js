973cae21fab6da783ac5686077c924d8657f9469
Your Name <you@example.com>
2023-05-27 16:43:24 +0200

    update

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function decrementarCS() {
 // Función para incrementar el contador
 function incrementarVOX() {
     var contadorElemento = document.getElementById("contadorVOX");
-    var contadorVOX= parseInt(contadorElemento.innerText);
+    var contadorVOX = parseInt(contadorElemento.innerText);
     contadorVOX++;
     contadorElemento.innerText = contadorVOX;
 }
@@ -87,63 +87,249 @@ function decrementarVOX() {
 }
 
 
-// Zona Compromis FALTAAAA
+// Zona Compromis 
 
 // Función para incrementar el contador
 function incrementarCOMPROMIS() {
-    var contadorElemento = document.getElementById("contador");
-    var contador = parseInt(contadorElemento.innerText);
-    contador++;
-    contadorElemento.innerText = contador;
+    var contadorElemento = document.getElementById("contadorCOMPROMIS");
+    var contadorCOMPROMIS = parseInt(contadorElemento.innerText);
+    contadorCOMPROMIS++;
+    contadorElemento.innerText = contadorCOMPROMIS;
 }
 
 // Función para decrementar el contador
-function decrementar() {
-    var contadorElemento = document.getElementById("contador");
-    var contador = parseInt(contadorElemento.innerText);
-    contador--;
-    contadorElemento.innerText = contador;
+function decrementarCOMPROMIS() {
+    var contadorElemento = document.getElementById("contadorCOMPROMIS");
+    var contadorCOMPROMIS = parseInt(contadorElemento.innerText);
+    contadorCOMPROMIS--;
+    contadorElemento.innerText = contadorCOMPROMIS;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Zona 
+// Zona RVPE
 
 // Función para incrementar el contador
-function incrementar() {
-    var contadorElemento = document.getElementById("contador");
-    var contador = parseInt(contadorElemento.innerText);
-    contador++;
-    contadorElemento.innerText = contador;
+function incrementarRVPE() {
+    var contadorElemento = document.getElementById("contadorRVPE");
+    var contadorRVPE = parseInt(contadorElemento.innerText);
+    contadorRVPE++;
+    contadorElemento.innerText = contadorRVPE;
 }
 
 // Función para decrementar el contador
-function decrementar() {
-    var contadorElemento = document.getElementById("contador");
-    var contador = parseInt(contadorElemento.innerText);
-    contador--;
-    contadorElemento.innerText = contador;
+function decrementarRVPE() {
+    var contadorElemento = document.getElementById("contadorRVPE");
+    var contadorRVPE = parseInt(contadorElemento.innerText);
+    contadorRVPE--;
+    contadorElemento.innerText = contadorRVPE;
+}
+
+
+// Zona PCPE
+
+// Función para incrementar el contador
+function incrementarPCPE() {
+    var contadorElemento = document.getElementById("contadorPCPE");
+    var contadorPCPE = parseInt(contadorElemento.innerText);
+    contadorPCPE++;
+    contadorElemento.innerText = contadorPCPE;
+}
+
+// Función para decrementar el contador
+function decrementarPCPE() {
+    var contadorElemento = document.getElementById("contadorPCPE");
+    var contadorPCPE = parseInt(contadorElemento.innerText);
+    contadorPCPE--;
+    contadorElemento.innerText = contadorPCPE;
+}
+
+
+// Zona Coalició Units COALICIONUNITS
+
+// Función para incrementar el contador
+function incrementarCOALICIONUNITS() {
+    var contadorElemento = document.getElementById("contadorCOALICIONUNITS");
+    var contadorCOALICIONUNITS = parseInt(contadorElemento.innerText);
+    contadorCOALICIONUNITS++;
+    contadorElemento.innerText = contadorCOALICIONUNITS;
+}
+
+// Función para decrementar el contador
+function decrementarCOALICIONUNITS() {
+    var contadorElemento = document.getElementById("contadorCOALICIONUNITS");
+    var contadorCOALICIONUNITS = parseInt(contadorElemento.innerText);
+    contadorCOALICIONUNITS--;
+    contadorElemento.innerText = contadorCOALICIONUNITS;
 }
 
 
 
+// Zona Alianza por el Comercio y la Vivienda
+
+// Función para incrementar el contador
+function incrementarALIANZACV() {
+    var contadorElemento = document.getElementById("contadorALIANZACV");
+    var contadorALIANZACV = parseInt(contadorElemento.innerText);
+    contadorALIANZACV++;
+    contadorElemento.innerText = contadorALIANZACV;
+}
+
+// Función para decrementar el contador
+function decrementarALIANZACV() {
+    var contadorElemento = document.getElementById("contadorALIANZACV");
+    var contadorALIANZACV = parseInt(contadorElemento.innerText);
+    contadorALIANZACV--;
+    contadorElemento.innerText = contadorALIANZACV;
+}
+
+
+// Zona Decidix
+
+// Función para incrementar el contador
+function incrementarDecidix() {
+    var contadorElemento = document.getElementById("contadorDecidix");
+    var contadorDecidix = parseInt(contadorElemento.innerText);
+    contadorDecidix++;
+    contadorElemento.innerText = contadorDecidix;
+}
+
+// Función para decrementar el contador
+function decrementarDecidix() {
+    var contadorElemento = document.getElementById("contadorDecidix");
+    var contadorDecidix = parseInt(contadorElemento.innerText);
+    contadorDecidix--;
+    contadorElemento.innerText = contadorDecidix;
+}
+
+
+// Zona Unides Podem-Esquerra Unida  UPEUPV
+
+// Función para incrementar el contador
+function incrementarUPEUPV() {
+    var contadorElemento = document.getElementById("contadorUPEUPV");
+    var contadorUPEUPV = parseInt(contadorElemento.innerText);
+    contadorUPEUPV++;
+    contadorElemento.innerText = contadorUPEUPV;
+}
+
+// Función para decrementar el contador
+function decrementarUPEUPV() {
+    var contadorElemento = document.getElementById("contadorUPEUPV");
+    var contadorUPEUPV = parseInt(contadorElemento.innerText);
+    contadorUPEUPV--;
+    contadorElemento.innerText = contadorUPEUPV;
+}
+
+// Zona Partido Animalista con el Medio Ambiente (PACMA)
+
+// Función para incrementar el contador
+function incrementarPACMA() {
+    var contadorElemento = document.getElementById("contadorPACMA");
+    var contadorPACMA = parseInt(contadorElemento.innerText);
+    contadorPACMA++;
+    contadorElemento.innerText = contadorPACMA;
+}
+
+// Función para decrementar el contador
+function decrementarPACMA() {
+    var contadorElemento = document.getElementById("contadorPACMA");
+    var contadorPACMA = parseInt(contadorElemento.innerText);
+    contadorPACMA--;
+    contadorElemento.innerText = contadorPACMA;
+}
+
+// Zona Esquerra Republicana del País Valencià (ERPV)
+
+// Función para incrementar el contador
+function incrementarERPV() {
+    var contadorElemento = document.getElementById("contadorERPV");
+    var contadorERPV = parseInt(contadorElemento.innerText);
+    contadorERPV++;
+    contadorElemento.innerText = contadorERPV;
+}
+
+// Función para decrementar el contador
+function decrementarERPV() {
+    var contadorElemento = document.getElementById("contadorERPV");
+    var contadorERPV = parseInt(contadorElemento.innerText);
+    contadorERPV--;
+    contadorElemento.innerText = contadorERPV;
+}
+
+
+// Zona Los Verdes-Ecopacifistas 
+
+// Función para incrementar el contador
+function incrementarLOSVERDES() {
+    var contadorElemento = document.getElementById("contadorLOSVERDES");
+    var contadorLOSVERDES = parseInt(contadorElemento.innerText);
+    contadorLOSVERDES++;
+    contadorElemento.innerText = contadorLOSVERDES;
+}
+
+// Función para decrementar el contador
+function decrementarLOSVERDES() {
+    var contadorElemento = document.getElementById("contadorLOSVERDES");
+    var contadorLOSVERDES = parseInt(contadorElemento.innerText);
+    contadorLOSVERDES--;
+    contadorElemento.innerText = contadorLOSVERDES;
+}
+
+
+// Zona Recortes Cero
+
+// Función para incrementar el contador
+function incrementarRecortesCero() {
+    var contadorElemento = document.getElementById("contadorRecortesCero");
+    var contadorRecortesCero = parseInt(contadorElemento.innerText);
+    contadorRecortesCero++;
+    contadorElemento.innerText = contadorRecortesCero;
+}
+
+// Función para decrementar el contador
+function decrementarRecortesCero() {
+    var contadorElemento = document.getElementById("contadorRecortesCero");
+    var contadorRecortesCero = parseInt(contadorElemento.innerText);
+    contadorRecortesCero--;
+    contadorElemento.innerText = contadorRecortesCero;
+}
+
+
+// Zona Escaños Blanco
+
+// Función para incrementar el contador
+function incrementarEscañosBlanco() {
+    var contadorElemento = document.getElementById("contadorEscañosBlanco");
+    var contadorEscañosBlanco = parseInt(contadorElemento.innerText);
+    contadorEscañosBlanco++;
+    contadorElemento.innerText = contadorEscañosBlanco;
+}
+
+// Función para decrementar el contador
+function decrementarEscañosBlanco() {
+    var contadorElemento = document.getElementById("contadorEscañosBlanco");
+    var contadorEscañosBlanco = parseInt(contadorElemento.innerText);
+    contadorEscañosBlanco--;
+    contadorElemento.innerText = contadorEscañosBlanco;
+}
+
+
+// Zona Unión Demócrata del Pueblo (UDP). 
+
+// Función para incrementar el contador
+function incrementarUDP() {
+    var contadorElemento = document.getElementById("contadorUDP");
+    var contadorUDP = parseInt(contadorElemento.innerText);
+    contadorUDP++;
+    contadorElemento.innerText = contadorUDP;
+}
+
+// Función para decrementar el contador
+function decrementarUDP() {
+    var contadorElemento = document.getElementById("contadorUDP");
+    var contadorUDP = parseInt(contadorElemento.innerText);
+    contadorUDP--;
+    contadorElemento.innerText = contadorUDP;
+}
 
 
 
@@ -152,19 +338,20 @@ function decrementar() {
 function abrirDialogo() {
     var modal = document.getElementById("modal");
     modal.style.display = "block";
-  }
+}
   
-  // Función para cerrar el diálogo
-  function cerrarDialogo() {
+// Función para cerrar el diálogo
+function cerrarDialogo() {
     var modal = document.getElementById("modal");
     modal.style.display = "none";
-  }
+}
   
-  // Función para guardar el nuevo título
-  function guardarTitulo() {
+// Función para guardar el nuevo título
+function guardarTitulo() {
     var nuevoTitulo = document.getElementById("nuevo-titulo").value;
     var tituloElemento = document.getElementById("titulo");
     tituloElemento.innerText = nuevoTitulo;
     cerrarDialogo();
-  }
+}
   
+
